b588b96866917e38ddf3d2ea7af43b6ffde6fdab
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-08-04 01:32:07 +0530

    chore: import user and rating data then renaming columns

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import seaborn as sns
 
 books = pd.read_csv('data/BX-Books.csv', sep=';', encoding="latin-1", on_bad_lines='skip')
 print(books.shape)
+
+# data cleaning
+books = books[['ISBN', 'Book-Title', 'Book-Author', 'Year-Of-Publication', 'Publisher', 'Image-URL-L']]
+books.rename(columns={
+    "Book-Title": "title",
+    "Book-Author": "author",
+    "Year-Of-Publication": "year",
+    "Publisher": "publisher",
+    "Image-URL-L": "image_url"
+}, inplace=True)
+print(books.columns)
+
+users = pd.read_csv('data/BX-Users.csv', sep=';', encoding="latin-1", on_bad_lines='skip')
+ratings = pd.read_csv('data/BX-Book-Ratings.csv', sep=';', encoding="latin-1", on_bad_lines='skip')
+ratings.rename(columns={
+    "User-ID": "user_id",
+    "Book-Rating": "rating",
+}, inplace=True)
+print(ratings.columns)
